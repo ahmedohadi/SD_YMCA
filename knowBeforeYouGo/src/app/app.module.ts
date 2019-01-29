7ac6b0bbe331e1e8eaf6354f14bbd101eae203bd
sglasford <sglasford@smu.edu>
2019-01-28 21:32:00 -0600

    Adding the Homepage and Navbar component

--- knowBeforeYouGo/src/app/app.module.ts
+++ knowBeforeYouGo/src/app/app.module.ts
@@ -8,21 +8,27 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contactus/contact.component';
 import { MroomComponent } from './Mroom/mroom.component';
 import { DrowningPreventionComponent } from './drowning-prevention/drowning-prevention.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutUsComponent,
-    ContactUsComponent,
-    MroomComponent,
-    DrowningPreventionComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    CommonModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      AboutUsComponent,
+      ContactUsComponent,
+      MroomComponent,
+      DrowningPreventionComponent,
+      HomePageComponent,
+      NavbarComponent
+   ],
+   imports: [
+      BrowserModule,
+      FormsModule,
+      CommonModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
