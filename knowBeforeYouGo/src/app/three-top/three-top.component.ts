7e8a35e0a1b6d8b6aaaef66e16703830b72ac5a1
Arely Alcantara <arely0715@gmail.com>
2019-03-27 00:26:49 -0500

    added top 3 rules component and made it responsive for all screens - weird thing for iphone5 so i made the banner scrollable

---
+++ knowBeforeYouGo/src/app/three-top/three-top.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-three-top',
+  templateUrl: './three-top.component.html',
+  styleUrls: ['./three-top.component.css']
+})
+export class ThreeTopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
