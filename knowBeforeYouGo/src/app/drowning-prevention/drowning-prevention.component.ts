5dbc5ccbe6bc09d6df6e6c52de8c65f7d49ebe7c
Ahmed Hadi <ahadi@smu.edu>
2019-04-11 00:52:22 -0500

    all items added preventsion page

--- knowBeforeYouGo/src/app/drowning-prevention/drowning-prevention.component.ts
+++ knowBeforeYouGo/src/app/drowning-prevention/drowning-prevention.component.ts
@@ -39,13 +39,15 @@ export class DrowningPreventionComponent implements OnInit {
 
 // sliders declaraions
 
-@ViewChild('local') elemRef: ElementRef;
+@ViewChild('local0') elemRef: ElementRef;
 @ViewChild('local1') elemRef1: ElementRef;
 @ViewChild('local2') elemRef2: ElementRef;
 @ViewChild('local3') elemRef3: ElementRef;
 @ViewChild('local4') elemRef4: ElementRef;
 @ViewChild('local5') elemRef5: ElementRef;
 @ViewChild('local6') elemRef6: ElementRef;
+@ViewChild('local7') elemRef7: ElementRef;
+
 
 
 
@@ -53,17 +55,14 @@ export class DrowningPreventionComponent implements OnInit {
 imgArr = [];
 imgTracker = 0;
 
-validator = false;
+validator0 = false;
 validator1 = false;
 validator2 = false;
 validator3 = false;
 validator4 = false;
 validator5 = false;
 validator6 = false;
-// validator7 = false;
-
-
-
+validator7 = false;
 
 
 // end slider declartions
@@ -80,81 +79,100 @@ validator6 = false;
   ngOnInit() {
     this.questions = this.quizzlerService.questions;
     this.rend.setStyle(this.elemRef.nativeElement, 'backgroundImage', 'url(http://media1.razorplanet.com/share/511646-7524/resources/2_image_1493237904_1233324_TrytheYWBimage.jpg');
-    this.validator = true;
+    this.validator0 = true;
   }
 
   updaetImages() {
     this.imgArr.push(this.imgTracker);
+
     if (this.imgTracker === this.imgArr[0]) {
-      this.rend.setStyle(this.elemRef1.nativeElement, 'backgroundImage', 'url(./../assets/DrowningStatistics.jpg');
-      this.validator = false;
+
+      this.rend.setStyle(this.elemRef.nativeElement, 'backgroundImage', 'url(./../../../assets/DrowningStatistics.jpg)');
+      this.validator0 = false;
       this.validator1 = true;
       this.validator2 = false;
       this.validator3 = false;
       this.validator4 = false;
       this.validator5 = false;
       this.validator6 = false;
+      this.validator7 = false;
+
 
     } else if (this.imgTracker === this.imgArr[1]) {
-      this.rend.setStyle(this.elemRef2.nativeElement, 'backgroundImage', 'url(./../assets/childrenIncreased.jpg)');
-      this.validator = false;
+      this.rend.setStyle(this.elemRef.nativeElement, 'backgroundImage', 'url(./../../../assets/childrenIncreased.jpg)');
+      this.validator0 = false;
       this.validator1 = false;
       this.validator2 = true;
       this.validator3 = false;
       this.validator4 = false;
       this.validator5 = false;
       this.validator6 = false;
+      this.validator7 = false;
+
 
     } else if (this.imgTracker === this.imgArr[2]) {
-      this.rend.setStyle(this.elemRef3.nativeElement, 'backgroundImage', 'url(./../assets/poolSafteyTips.jpg)');
-      this.validator = false;
+      this.rend.setStyle(this.elemRef.nativeElement, 'backgroundImage', 'url(./../../../assets/poolSafteyTips.jpg)');
+      this.validator0 = false;
       this.validator1 = false;
       this.validator2 = false;
       this.validator3 = true;
       this.validator4 = false;
       this.validator5 = false;
       this.validator6 = false;
+      this.validator7 = false;
+
 
     } else if (this.imgTracker === this.imgArr[3]) {
-      this.rend.setStyle(this.elemRef4.nativeElement, 'backgroundImage', 'url(./../assets/preparToSwim.jpg)');
-      this.validator = false;
+      this.rend.setStyle(this.elemRef.nativeElement, 'backgroundImage', 'url(./../../../assets/preparToSwim.jpg)');
+      this.validator0 = false;
       this.validator1 = false;
       this.validator2 = false;
       this.validator3 = false;
       this.validator4 = true;
       this.validator5 = false;
       this.validator6 = false;
+      this.validator7 = false;
+
 
     } else if (this.imgTracker === this.imgArr[4]) {
-      this.rend.setStyle(this.elemRef5.nativeElement, 'backgroundImage', 'url(./../assets/Whenyourpoolisbeingused.jpg)');
-      this.validator = false;
+      this.rend.setStyle(this.elemRef.nativeElement, 'backgroundImage', 'url(./../../../assets/Whenyourpoolisbeingused.jpg)');
+      this.validator0 = false;
       this.validator1 = false;
       this.validator2 = false;
       this.validator3 = false;
       this.validator4 = false;
       this.validator5 = true;
       this.validator6 = false;
+      this.validator7 = false;
+
 
     } else if (this.imgTracker === this.imgArr[5]) {
-      this.rend.setStyle(this.elemRef6.nativeElement, 'backgroundImage', 'url(./../assets/Whenyourpoolisnotinuse.jpg)');
-      this.validator = false;
+      this.rend.setStyle(this.elemRef.nativeElement, 'backgroundImage', 'url(./../../../assets/Whenyourpoolisnotinuse.jpg)');
+      this.validator0 = false;
       this.validator1 = false;
       this.validator2 = false;
       this.validator3 = false;
       this.validator4 = false;
       this.validator5 = false;
       this.validator6 = true;
+      this.validator7 = false;
       this.reset();
     }
+    this.imgTracker++;
+
     console.log('length', this.imgArr.length);
     console.log('tracker', this.imgTracker);
-    this.imgTracker++;
+
+    }
+    reset() {
+      this.imgTracker = 0;
+    this.imgArr.length = 0;
+  }
 }
-reset() {
-  this.imgTracker = 0;
-  this.imgArr.length = 0;
-}
+
+
+
 // onAnimate() {
   //   this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
   // }
-}
+
