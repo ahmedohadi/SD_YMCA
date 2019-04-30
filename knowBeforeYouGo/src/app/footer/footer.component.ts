c729cd8c11c82fbf3c253ad74f3858cdd9386d93
Ahmed Hadi <ahadi@smu.edu>
2019-04-30 01:35:56 -0500

    updates ng cli

--- knowBeforeYouGo/src/app/footer/footer.component.ts
+++ knowBeforeYouGo/src/app/footer/footer.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
   // @Input()
   // public home: any;
